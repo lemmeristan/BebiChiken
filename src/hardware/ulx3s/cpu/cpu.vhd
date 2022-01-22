1b58d534120ea6e0c1622436828fca34e8f20cab
Lemmer El Assal <lemmerelassal@icloud.com>
2022-01-22 18:50:23 +0100

    Out of order execution using DP16KD working. Next step: generalize DP16KD instantiation.

--- src/hardware/ulx3s/cpu/cpu.vhd
+++ src/hardware/ulx3s/cpu/cpu.vhd
@@ -13,17 +13,17 @@ ENTITY cpu IS
 
         -- Instruction memory bus
         inst_width : OUT STD_LOGIC_VECTOR(1 DOWNTO 0); -- "00" -> 1 byte, "01" -> 2 bytes, "10" -> 4 bytes, "11" -> invalid / 8 bytes for RV64
-        inst_addr  : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        inst_addr : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
         inst_rdata : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        inst_re    : OUT STD_LOGIC;
-        inst_rdy   : IN STD_LOGIC;
+        inst_re : OUT STD_LOGIC;
+        inst_rdy : IN STD_LOGIC;
 
         -- Data memory bus
-        data_width            : OUT STD_LOGIC_VECTOR(1 DOWNTO 0); -- "00" -> 1 byte, "01" -> 2 bytes, "10" -> 4 bytes, "11" -> invalid / 8 bytes for RV64
+        data_width : OUT STD_LOGIC_VECTOR(1 DOWNTO 0); -- "00" -> 1 byte, "01" -> 2 bytes, "10" -> 4 bytes, "11" -> invalid / 8 bytes for RV64
         data_addr, data_wdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        data_rdata            : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        data_re, data_we      : OUT STD_LOGIC;
-        data_rdy, data_wack   : IN STD_LOGIC
+        data_rdata : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        data_re, data_we : OUT STD_LOGIC;
+        data_rdy, data_wack : IN STD_LOGIC
 
     );
 END cpu;
@@ -55,19 +55,19 @@ ARCHITECTURE behavioural OF cpu IS
 
     --ATTRIBUTE syn_keep : BOOLEAN;
 
-    SIGNAL update_pc, update_pc_branch, update_pc_branch_r    : STD_LOGIC;
-    SIGNAL branch_next_pc                                     : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL update_pc, update_pc_branch, update_pc_branch_r : STD_LOGIC;
+    SIGNAL branch_next_pc : STD_LOGIC_VECTOR(31 DOWNTO 0);
     SIGNAL writeback_data, writeback_token, writeback_next_pc : opcode_group_word_t;
-    SIGNAL eu_we, eu_we_r, eu_we_r_r, eu_busy, writeback_we   : opcode_group_bit_t;
-    SIGNAL allready                                           : STD_LOGIC;
+    SIGNAL eu_we, eu_we_r, eu_we_r_r, eu_busy, writeback_we : opcode_group_bit_t;
+    SIGNAL allready : STD_LOGIC;
 
     SIGNAL writeback_rd, rd_out : opcode_group_regidx_t;
 
     SIGNAL update_rd : STD_LOGIC;
 
-    SIGNAL regfile_rd                                                                                                                                                                                                                                         : STD_LOGIC_VECTOR(4 DOWNTO 0);
-    SIGNAL rd_data_in                                                                                                                                                                                                                                         : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL writeback_update_pc                                                                                                                                                                                                                                : opcode_group_bit_t;
+    SIGNAL regfile_rd : STD_LOGIC_VECTOR(4 DOWNTO 0);
+    SIGNAL rd_data_in : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL writeback_update_pc : opcode_group_bit_t;
     SIGNAL rs1_data, rs1_data_r, rs2_data, rs2_data_r, next_pc, inst_rdata_r, inst_rdata_r_r, rs1_data_out, rs2_data_out, rs1_data_out_r, rs2_data_out_r, rs1_data_out_r_r, rs2_data_out_r_r, rs1_data_out_r_r_r, rs2_data_out_r_r_r, token_r, token, n_token : STD_LOGIC_VECTOR(31 DOWNTO 0); -- n_pc
     --SIGNAL rs1_data_in, rs2_data_in, instruction_in, pc_in                                                                                                         : opcode_group_word_t;
 
@@ -75,19 +75,19 @@ ARCHITECTURE behavioural OF cpu IS
 
     --SIGNAL inst_rdy_r, update_pc_r : STD_LOGIC;
 
-    ALIAS funct7       : STD_LOGIC_VECTOR(6 DOWNTO 0) IS inst_rdata(31 DOWNTO 25);
-    ALIAS rs2          : STD_LOGIC_VECTOR(4 DOWNTO 0) IS inst_rdata(24 DOWNTO 20);
-    ALIAS rs1          : STD_LOGIC_VECTOR(4 DOWNTO 0) IS inst_rdata(19 DOWNTO 15);
-    ALIAS funct3       : STD_LOGIC_VECTOR(2 DOWNTO 0) IS inst_rdata(14 DOWNTO 12);
-    ALIAS rd           : STD_LOGIC_VECTOR(4 DOWNTO 0) IS inst_rdata(11 DOWNTO 7);
-    ALIAS opcode       : STD_LOGIC_VECTOR(6 DOWNTO 0) IS inst_rdata(6 DOWNTO 0);
-    SIGNAL initialized : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    ALIAS funct7 : STD_LOGIC_VECTOR(6 DOWNTO 0) IS inst_rdata(31 DOWNTO 25);
+    ALIAS rs2 : STD_LOGIC_VECTOR(4 DOWNTO 0) IS inst_rdata(24 DOWNTO 20);
+    ALIAS rs1 : STD_LOGIC_VECTOR(4 DOWNTO 0) IS inst_rdata(19 DOWNTO 15);
+    ALIAS funct3 : STD_LOGIC_VECTOR(2 DOWNTO 0) IS inst_rdata(14 DOWNTO 12);
+    ALIAS rd : STD_LOGIC_VECTOR(4 DOWNTO 0) IS inst_rdata(11 DOWNTO 7);
+    ALIAS opcode : STD_LOGIC_VECTOR(6 DOWNTO 0) IS inst_rdata(6 DOWNTO 0);
+    SIGNAL initialized : STD_LOGIC_VECTOR(15 DOWNTO 0);
 
     ALIAS funct7_r : STD_LOGIC_VECTOR(6 DOWNTO 0) IS inst_rdata_r(31 DOWNTO 25);
-    ALIAS rs2_r    : STD_LOGIC_VECTOR(4 DOWNTO 0) IS inst_rdata_r(24 DOWNTO 20);
-    ALIAS rs1_r    : STD_LOGIC_VECTOR(4 DOWNTO 0) IS inst_rdata_r(19 DOWNTO 15);
+    ALIAS rs2_r : STD_LOGIC_VECTOR(4 DOWNTO 0) IS inst_rdata_r(24 DOWNTO 20);
+    ALIAS rs1_r : STD_LOGIC_VECTOR(4 DOWNTO 0) IS inst_rdata_r(19 DOWNTO 15);
     ALIAS funct3_r : STD_LOGIC_VECTOR(2 DOWNTO 0) IS inst_rdata_r(14 DOWNTO 12);
-    ALIAS rd_r     : STD_LOGIC_VECTOR(4 DOWNTO 0) IS inst_rdata_r(11 DOWNTO 7);
+    ALIAS rd_r : STD_LOGIC_VECTOR(4 DOWNTO 0) IS inst_rdata_r(11 DOWNTO 7);
     ALIAS opcode_r : STD_LOGIC_VECTOR(6 DOWNTO 0) IS inst_rdata_r(6 DOWNTO 0);
 
     -- dispatcher
@@ -100,39 +100,39 @@ ARCHITECTURE behavioural OF cpu IS
     SIGNAL lock_rd, lock_pc, rs1_locked, rs2_locked, pc_locked, pc_locked_r, updates_pc : STD_LOGIC;
 
     SIGNAL pc, n_pc, n_inst_rdata_r, pc_r : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL pc_owner, n_pc_owner           : opcode_group_t;
+    SIGNAL pc_owner, n_pc_owner : opcode_group_t;
     TYPE whole_state_t IS (ws0, ws1, ws2, ws3, ws4, ws5, ws6, ws7);
     SIGNAL wstate, n_wstate : whole_state_t;
 
-    signal rs1_locked_r, rs2_locked_r : std_logic;
+    SIGNAL rs1_locked_r, rs2_locked_r : STD_LOGIC;
 
-    SIGNAL inc_token : STD_LOGIC;
+    SIGNAL inc_token, reset_init : STD_LOGIC;
 
 BEGIN
     PROCESS (wstate, initialized, inst_rdata_r, inst_rdata, rs1_locked, rs2_locked, eu_busy, imm_decoded, pc, token, pc_owner, writeback_update_pc, writeback_next_pc, rs1_locked_r, rs2_locked_r, rd_r)
     BEGIN
-        n_wstate                       <= wstate;
-        n_inst_rdata_r                 <= inst_rdata_r;
+        n_wstate <= wstate;
         writeback_data(OPCODE_INVALID) <= (OTHERS => '0');
-        writeback_we(OPCODE_INVALID)   <= '0';
-        eu_we                          <= (OTHERS => '0');
+        writeback_we(OPCODE_INVALID) <= '0';
+        eu_we <= (OTHERS => '0');
 
         writeback_rd(OPCODE_INVALID) <= rd_r;
-        lock_rd                      <= '0';
-        new_rd_lock_owner            <= OPCODE_INVALID;
+        lock_rd <= '0';
+        new_rd_lock_owner <= OPCODE_INVALID;
 
         writeback_token(OPCODE_INVALID) <= token;
 
-        n_pc                           <= pc;
-        n_token                        <= token;
-        n_pc_owner                     <= pc_owner;
-        inc_token                      <= '0';
+        n_pc <= pc;
+        n_token <= token;
+        n_pc_owner <= pc_owner;
+        inc_token <= '0';
+        reset_init <= '0';
 
         CASE wstate IS
             WHEN ws0 => -- set instruction register
-                IF (initialized = X"FF") THEN
-                    n_wstate       <= ws1;
-                    n_inst_rdata_r <= inst_rdata;
+                IF (initialized = X"FFFF") THEN
+                    n_wstate <= ws1;
+                    reset_init <= '1';
                 END IF;
             WHEN ws1 => -- execute / dispatch
 
@@ -147,14 +147,14 @@ BEGIN
                     CASE f_decode_opcode(inst_rdata_r) IS
                         WHEN OPCODE_U_TYPE_LUI =>
                             writeback_data(OPCODE_INVALID) <= imm_decoded;
-                            writeback_we(OPCODE_INVALID)   <= '1';
+                            writeback_we(OPCODE_INVALID) <= '1';
                         WHEN OPCODE_U_TYPE_AUIPC =>
                             writeback_data(OPCODE_INVALID) <= pc + imm_decoded;
-                            writeback_we(OPCODE_INVALID)   <= '1';
+                            writeback_we(OPCODE_INVALID) <= '1';
                         WHEN OTHERS =>
                             IF (f_decode_exec_unit(inst_rdata_r) = OPCODE_I_TYPE) AND (f_uses_rs1(inst_rdata_r) = '0') THEN
                                 writeback_data(OPCODE_INVALID) <= f_calculate_i_type_zero(inst_rdata_r);
-                                writeback_we(OPCODE_INVALID)   <= '1';
+                                writeback_we(OPCODE_INVALID) <= '1';
                             ELSE
                                 IF f_updates_rd(inst_rdata_r) = '1' THEN
                                     new_rd_lock_owner <= f_decode_exec_unit(inst_rdata_r);
@@ -164,19 +164,20 @@ BEGIN
                     END CASE;
                     n_token <= token + X"00000001";
                     IF f_updates_pc(inst_rdata_r) = '1' THEN
-                        n_wstate   <= ws2;
+                        n_wstate <= ws2;
                         n_pc_owner <= f_decode_exec_unit(inst_rdata_r);
                     ELSE
                         n_wstate <= ws0;
-                        n_pc     <= pc + X"00000004";
+                        n_pc <= pc + X"00000004";
                     END IF;
                 END IF;
 
             WHEN ws2 =>
                 IF (writeback_update_pc(pc_owner) = '1') THEN
-                    n_pc       <= writeback_next_pc(pc_owner);
+                    n_pc <= writeback_next_pc(pc_owner);
                     n_pc_owner <= OPCODE_INVALID;
-                    n_wstate   <= ws0;
+                    n_wstate <= ws0;
+                    reset_init <= '1';
                 END IF;
 
             WHEN OTHERS =>
@@ -186,16 +187,18 @@ BEGIN
 
     updates_pc <= f_updates_pc(inst_rdata);
 
-    decoded   <= f_decode_exec_unit(inst_rdata);
+    decoded <= f_decode_exec_unit(inst_rdata);
     decoded_r <= f_decode_exec_unit(inst_rdata_r);
 
-    inst_addr  <= pc;
-    inst_re    <= '1';
+    inst_addr <= pc;
+    inst_re <= '1';
     inst_width <= "10"; -- unused
 
-    rd_out(OPCODE_INVALID)  <= "00000";
+    rd_out(OPCODE_INVALID) <= "00000";
     eu_busy(OPCODE_INVALID) <= '0';
     --eu_busy(OPCODE_U_TYPE)  <= '0';
+
+    imm_decoded <= f_decode_imm(inst_rdata_r);
 
     PROCESS (rst, clk)
     BEGIN
@@ -206,24 +209,20 @@ BEGIN
         END IF;
 
         IF rst = '1' THEN
-        inst_rdata_r <= (OTHERS => '0');
+            inst_rdata_r <= (OTHERS => '0');
 
             wstate <= ws0;
-            --inst_rdata_r     <= (OTHERS => '0');
-
-            initialized      <= (OTHERS => '0');
-            fetcher_state    <= FETCHER_STATE_S0;
+            initialized <= (OTHERS => '0');
+            fetcher_state <= FETCHER_STATE_S0;
             dispatcher_state <= DISPATCHER_STATE_S0;
-            eu_we_r          <= (OTHERS => '0');
-            rs1_data_r       <= (OTHERS => '0');
-            rs2_data_r       <= (OTHERS => '0');
-            imm_decoded      <= (OTHERS => '0');
-            token            <= X"DEADBEEF";
+            eu_we_r <= (OTHERS => '0');
+            rs1_data_r <= (OTHERS => '0');
+            rs2_data_r <= (OTHERS => '0');
+            --imm_decoded      <= (OTHERS => '0');
+            token <= X"DEADBEEF";
 
             pc_owner <= OPCODE_INVALID;
-            pc       <= entry_point;
-
-            inst_rdata_r_valid <= '0';
+            pc <= entry_point;
 
             pc_r <= entry_point;
 
@@ -234,10 +233,10 @@ BEGIN
             rs2_locked_r <= '0';
 
         ELSIF rising_edge(clk) THEN
-        inst_rdata_r <= inst_rdata;
+            inst_rdata_r <= inst_rdata;
 
-        rs1_locked_r <= rs1_locked;
-        rs2_locked_r <= rs2_locked;
+            rs1_locked_r <= rs1_locked;
+            rs2_locked_r <= rs2_locked;
 
             wstate <= n_wstate;
             --inst_rdata_r <= n_inst_rdata_r;
@@ -252,25 +251,23 @@ BEGIN
             rs2_data_out_r_r_r <= rs2_data_out_r_r;
 
             pc_owner <= n_pc_owner;
-
-            imm_decoded      <= f_decode_imm(inst_rdata);
-            fetcher_state    <= n_fetcher_state;
+            fetcher_state <= n_fetcher_state;
             dispatcher_state <= n_dispatcher_state;
 
-            eu_we_r    <= eu_we;
-            eu_we_r_r  <= eu_we_r;
+            eu_we_r <= eu_we;
+            eu_we_r_r <= eu_we_r;
             rs1_data_r <= rs1_data;
             rs2_data_r <= rs2_data;
 
-            initialized <= initialized(6 DOWNTO 0) & inst_rdy;
-            token       <= n_token;
-            token_r     <= token;
-            pc          <= n_pc;
+            token <= n_token;
+            pc <= n_pc;
 
             pc_r <= pc;
-            IF (issue = '1') THEN
-                inst_rdata_r_valid <= '1';
 
+            IF reset_init = '1' THEN
+                initialized <= (OTHERS => '0');
+            ELSE
+                initialized <= initialized(14 DOWNTO 0) & inst_rdy;
             END IF;
         END IF;
     END PROCESS;
@@ -281,18 +278,18 @@ BEGIN
         clk => clk, rst => rst,
         rs1 => rs1_r, rs2 => rs2_r, rd => rd_r,
 
-        lock_rd           => lock_rd,           --lock_pc => lock_pc,
+        lock_rd => lock_rd, --lock_pc => lock_pc,
         new_rd_lock_owner => new_rd_lock_owner, --new_pc_lock_owner => new_pc_lock_owner,
-        lock_token        => token,
+        lock_token => token,
 
-        writeback_we   => writeback_we, --writeback_update_pc => writeback_update_pc,
+        writeback_we => writeback_we, --writeback_update_pc => writeback_update_pc,
         writeback_data => writeback_data,
         --writeback_next_pc => writeback_next_pc, 
         writeback_token => writeback_token,
-        writeback_rd    => writeback_rd,
+        writeback_rd => writeback_rd,
 
         rs1_data_out => rs1_data_out, rs2_data_out => rs2_data_out, --pc => regfile_pc,
-        rs1_locked => rs1_locked, rs2_locked => rs2_locked          --, pc_locked => pc_locked
+        rs1_locked => rs1_locked, rs2_locked => rs2_locked --, pc_locked => pc_locked
     );
     i_eu_mem : eu_mem
     PORT MAP(
@@ -301,13 +298,13 @@ BEGIN
         we => eu_we(OPCODE_MEM_TYPE),
         rs1_data => rs1_data_out, rs2_data => rs2_data_out, instruction => inst_rdata_r, token => token, imm => imm_decoded,
 
-        writeback_we    => writeback_we(OPCODE_MEM_TYPE),
-        writeback_data  => writeback_data(OPCODE_MEM_TYPE),
+        writeback_we => writeback_we(OPCODE_MEM_TYPE),
+        writeback_data => writeback_data(OPCODE_MEM_TYPE),
         writeback_token => writeback_token(OPCODE_MEM_TYPE),
-        writeback_rd    => writeback_rd(OPCODE_MEM_TYPE),
+        writeback_rd => writeback_rd(OPCODE_MEM_TYPE),
         mem_wack => data_wack, mem_rdy => data_rdy, mem_rdata => data_rdata, mem_wdata => data_wdata, mem_addr => data_addr, mem_width => data_width,
         mem_re => data_re, mem_we => data_we,
-        busy            => eu_busy(OPCODE_MEM_TYPE)
+        busy => eu_busy(OPCODE_MEM_TYPE)
     );
     i_eu_branch : eu_branch_type
     PORT MAP(
@@ -316,13 +313,13 @@ BEGIN
         we => eu_we(OPCODE_BRANCH_TYPE),
         rs1_data => rs1_data_out, rs2_data => rs2_data_out, instruction => inst_rdata_r, pc => pc, token => token, imm => imm_decoded,
 
-        writeback_we        => writeback_we(OPCODE_BRANCH_TYPE),
-        writeback_next_pc   => writeback_next_pc(OPCODE_BRANCH_TYPE),
-        writeback_data      => writeback_data(OPCODE_BRANCH_TYPE),
-        writeback_token     => writeback_token(OPCODE_BRANCH_TYPE),
-        writeback_rd        => writeback_rd(OPCODE_BRANCH_TYPE),
+        writeback_we => writeback_we(OPCODE_BRANCH_TYPE),
+        writeback_next_pc => writeback_next_pc(OPCODE_BRANCH_TYPE),
+        writeback_data => writeback_data(OPCODE_BRANCH_TYPE),
+        writeback_token => writeback_token(OPCODE_BRANCH_TYPE),
+        writeback_rd => writeback_rd(OPCODE_BRANCH_TYPE),
         writeback_update_pc => writeback_update_pc(OPCODE_BRANCH_TYPE),
-        busy                => eu_busy(OPCODE_BRANCH_TYPE)
+        busy => eu_busy(OPCODE_BRANCH_TYPE)
     );
 
     i_eu_i : eu_i_type
@@ -332,11 +329,11 @@ BEGIN
         we => eu_we(OPCODE_I_TYPE),
         rs1_data => rs1_data_out, instruction => inst_rdata_r, token => token, imm => imm_decoded,
 
-        writeback_we    => writeback_we(OPCODE_I_TYPE),
-        writeback_data  => writeback_data(OPCODE_I_TYPE),
+        writeback_we => writeback_we(OPCODE_I_TYPE),
+        writeback_data => writeback_data(OPCODE_I_TYPE),
         writeback_token => writeback_token(OPCODE_I_TYPE),
-        writeback_rd    => writeback_rd(OPCODE_I_TYPE),
-        busy            => eu_busy(OPCODE_I_TYPE)
+        writeback_rd => writeback_rd(OPCODE_I_TYPE),
+        busy => eu_busy(OPCODE_I_TYPE)
 
     );
 
@@ -347,12 +344,12 @@ BEGIN
         we => eu_we(OPCODE_R_TYPE),
         rs1_data => rs1_data_out, rs2_data => rs2_data_out, instruction => inst_rdata_r, token => token,
 
-        writeback_we    => writeback_we(OPCODE_R_TYPE),
-        writeback_data  => writeback_data(OPCODE_R_TYPE),
+        writeback_we => writeback_we(OPCODE_R_TYPE),
+        writeback_data => writeback_data(OPCODE_R_TYPE),
         writeback_token => writeback_token(OPCODE_R_TYPE),
 
         writeback_rd => writeback_rd(OPCODE_R_TYPE),
-        busy         => eu_busy(OPCODE_R_TYPE)
+        busy => eu_busy(OPCODE_R_TYPE)
     );
 
 END behavioural;
--- src/hardware/ulx3s/makeit.sh
+++ src/hardware/ulx3s/makeit.sh
@@ -1,20 +1,20 @@
 PROJECTNAME=bebichiken
 
-HDMI_VERILOGS=(HDMI_clock.v HDMI_test_hires.v TMDS_encoder.v) 
+HDMI_VERILOGS=(HDMI_clock.v HDMI_test_hires.v TMDS_encoder.v) #() #
 SDRAM_VERILOGS=(dualport_ram.v) #() #
 SPIFLASH_VERILOGS=(simple_dualport_ram_8k_lattice.v)
 PERIPHERALS_VERILOGS=("${HDMI_VERILOGS[@]/#/peripherals/hdmi/} ${SDRAM_VERILOGS[@]/#/peripherals/sdram/} ${SPIFLASH_VERILOGS[@]/#/peripherals/spiflash/}")
 
 
-SDRAM_VHDLS=(sdram_cache.vhd) #() #
+SDRAM_VHDLS=(sdram_cache.vhd) 
 SPIFLASH_VHDLS=(quadflash_cache.vhd)
 UART_VHDLS=(uart.vhd)
 PERIPHERALS_VHDLS=("${UART_VHDLS[@]/#/peripherals/uart/} ${SDRAM_VHDLS[@]/#/peripherals/sdram/} ${SPIFLASH_VHDLS[@]/#/peripherals/spiflash/}")
 
 
 
-VERILOGS="${PERIPHERALS_VERILOGS[@]}" # HDMI_test_hires.v HDMI_clock.v TMDS_encoder.v"
-VHDLS="datatypes.vhd top.vhd cpu/cpu.vhd cpu/regfile_half.vhd cpu/eu_mem.vhd mmu.vhd cpu/eu_i_type.vhd cpu/eu_branch_type.vhd cpu/eu_r_type.vhd cpu/eu_u_type.vhd   ${PERIPHERALS_VHDLS[@]}" #  gpio.vhd timebase.vhd hdmi.vhd ram.vhd " # cpu/eu_b_type.vhd cpu/eu_i_type.vhd cpu/eu_j_type.vhd cpu/eu_r_type.vhd cpu/eu_u_type.vhd 
+VERILOGS="${PERIPHERALS_VERILOGS[@]} cpu/regfile_dpram/dp16kd_wrapper.v  cpu/regfile_dpram/dpram_regfile_lattice.v " # HDMI_test_hires.v HDMI_clock.v TMDS_encoder.v"
+VHDLS="datatypes.vhd top.vhd cpu/cpu.vhd cpu/regfile_dpram/regfile_dpram.vhd cpu/eu_mem.vhd mmu.vhd cpu/eu_i_type.vhd cpu/eu_branch_type.vhd cpu/eu_r_type.vhd    ${PERIPHERALS_VHDLS[@]}" # cpu/eu_u_type.vhd cpu/regfile_half.vhd  gpio.vhd timebase.vhd hdmi.vhd ram.vhd " # cpu/eu_b_type.vhd cpu/eu_i_type.vhd cpu/eu_j_type.vhd cpu/eu_r_type.vhd cpu/eu_u_type.vhd 
 
 if [ $1 == "clean" ]; then
   rm -f *.bit *.json *.config *.svf *~
